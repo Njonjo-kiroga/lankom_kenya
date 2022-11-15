966f0d42f947e0f8ee496d48c87c56486f579d79
Njonjo-kiroga <andrewnjonjo@gmail.com>
2022-11-15 07:30:45 +0300

    main site

---
+++ main/assets/js/fireabase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuvMjprgF4A28MP6CL0mg9hD1jlvZ5RJA",
+  authDomain: "lankom-kenya.firebaseapp.com",
+  projectId: "lankom-kenya",
+  storageBucket: "lankom-kenya.appspot.com",
+  messagingSenderId: "428201135533",
+  appId: "1:428201135533:web:59cd04598d8d61d772e524",
+  measurementId: "G-SP4851NN7K"
+};
+
+firebase.initializeApp(firebaseConfig);
